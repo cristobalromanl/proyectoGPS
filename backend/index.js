74d23b3c032273263086481407f44d72d459d37c
Nicolas Silva <njsilvapereira@gmail.com>
2023-04-10 23:22:28 -0400

    probando

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
+
 dotenv = require('dotenv')
 dotenv.config();
 mongoose.set('strictQuery', true);
