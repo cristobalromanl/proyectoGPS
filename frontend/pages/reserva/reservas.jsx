cce4095449dc4e2964b6c003f01ffec584477e96
Nicolas Silva Pereira <njsilvapereira@gmail.com>
2023-05-19 01:38:59 -0400

    pagina reserva casi lista

--- frontend/pages/reserva/reservas.jsx
+++ frontend/pages/reserva/reservas.jsx
@@ -1,57 +1,82 @@
 import React from 'react'
 import {SlOptionsVertical} from 'react-icons/sl'
 import {BsHouseDoor} from 'react-icons/bs'
-import Logo from './mbape.jpg'
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import {
     Box,
     Button,
-    Flex,
     Heading,
     Icon,
     Image,
     Spacer,
-    BackgroundImage,
+    Flex,
     VStack,
     Container,
     HStack,
     Text
   } from '@chakra-ui/react'
 
- 
+  
 const reservas= ()=> {
   return (
     <Container  margin={'0'} p={'0'} maxW={'full'} maxH={'full'}>
-      <Image src='Sportify.jpg'/>
       <HStack as="nav" p="10px" bg="#5D3C81">
-        <Heading color={'white'} >Sportify</Heading>
+        <Heading color={'white'}><Image src='/Sportify.png' w={'110px'} h={'100'}/></Heading>
         <Spacer/>
-        <Button colorScheme='teal' variant='unstyled' color={'white'} mx="20px">
-        <Icon as={BsHouseDoor} color={'white'} mx="2px"/>
-          Inicio
+        <Button colorScheme='teal' variant='unstyled' color={'white'} mx="100px" fontSize={'28px'} display={'flex'} >
+        <Icon as={BsHouseDoor} color={'white'} w={'48px'} h={'45px'}/>
+         <Text w={'79px'} h={'42'}>Inicio</Text>
         </Button>
-        <Button colorScheme='teal' variant='unstyled' color={'white'} mx="10px">
-        <Box><Icon as={SlOptionsVertical} color={'white'}/></Box>
+        <Button colorScheme='teal' variant='unstyled' color={'white'} mx="20px">
+        <Box><Icon w={'45px'} h={'45px'} as={SlOptionsVertical} color={'white'}/></Box>
         </Button>
       </HStack>
-      <VStack bg={"gray.300"} width={'full'} height={' container.lg'}>
+      <VStack bg={"#c08ee1"} width={'full'} height={' container.lg'}>
         <Heading my={'30px'} p={'15px'}>Selecciona el deporte </Heading>
         <VStack spacing={'30px'} my={'25px'}>
         <HStack spacing={'30px'}> 
-          <Box bg={'red.300'} w={'250px'} h={'256px'} onClick={()=>{alert("futbol")}}>futbolito</Box>
-          <Card bg={'red.300'} w={'250px'} h={'256px'}>
-            <CardHeader>
-              <Heading textAlign="center">Tenis</Heading>
-              <Box>
-              <Image src='Sportify.jpg'/>
-              </Box>
-            </CardHeader>
+        <Card bg={'#c08ee1'} w={'250px'} h={'252px'}transition={['0.3s', 'ease']}  boxShadow={'dark-lg'} cursor={'pointer'} onClick={()=>{console.log("Futbolito")}}>
+            <CardBody>
+              <Flex direction={'column'} alignContent={'center'} justify={'center'}>
+                <Heading textAlign="center">Futbolito</Heading>
+                <Image mx={'29px'} h={'130px'} w={'140px'} src='https://cdn.discordapp.com/attachments/723251921393025028/1108953429147131955/asfasdf.png'/>
+              </Flex> 
+            </CardBody>
           </Card>
-          <Box bg={'red.300'} w={'250px'} h={'256px'}>BasketBall</Box>
+          <Card bg={'#c08ee1'} w={'250px'} h={'252px'}transition={['0.3s', 'ease']}  boxShadow={'dark-lg'} cursor={'pointer'} onClick={()=>{console.log("Tenis")}}>
+            <CardBody>
+              <Flex direction={'column'} alignContent={'center'} justify={'center'} >
+                <Heading textAlign="center">Tenis</Heading>
+                <Image mx={'29px'} h={'130px'} w={'140px'} src='https://cdn.discordapp.com/attachments/723251921393025028/1108953502291599411/tenis.png'/>
+              </Flex> 
+            </CardBody>
+          </Card>
+          <Card bg={'#c08ee1'} w={'250px'} h={'252px'} transition={['0.3s', 'ease']}  boxShadow={'dark-lg'} cursor={'pointer'}>
+            <CardBody>
+              <Flex direction={'column'} alignContent={'center'} justify={'center'} onClick={()=>{console.log("Basketball")}}>
+                <Heading textAlign="center">Basketball</Heading>
+                <Image mx={'29px'} h={'130px'} w={'140px'} src='https://cdn.discordapp.com/attachments/723251921393025028/1108953551474004058/i.png'/>
+              </Flex> 
+            </CardBody>
+          </Card>
         </HStack>
         <HStack spacing={'30px'}>
-          <Box bg={'red.300'} w={'250px'} h={'256px'}>Padel</Box>
-          <Box bg={'red.300'} w={'250px'} h={'256px'}>Voleibol</Box>
+        <Card bg={'#c08ee1'} w={'250px'} h={'252px'} transition={['0.3s', 'ease']}  boxShadow={'dark-lg'} cursor={'pointer'} onClick={()=>{console.log("Padel")}}>
+            <CardBody>
+              <Flex direction={'column'} alignContent={'center'} justify={'center'}>
+                <Heading textAlign="center">Padel</Heading>
+                <Image mx={'56px'} h={'130px'} w={'140px'} src='https://cdn.discordapp.com/attachments/723251921393025028/1108953751391309874/raqueta-de-padel.png'/>
+              </Flex> 
+            </CardBody>
+          </Card>
+          <Card bg={'#c08ee1'} w={'250px'} h={'252px'} transition={['0.3s', 'ease']}  boxShadow={'dark-lg'} cursor={'pointer'} onClick={()=>{console.log("Voley")}}>
+            <CardBody>
+              <Flex direction={'column'} alignContent={'center'} justify={'center'}>
+                <Heading textAlign="center">Voley</Heading>
+                <Image mx={'29px'} h={'130px'} w={'140px'} src='https://cdn.discordapp.com/attachments/723251921393025028/1108953959995035688/cuadra.png'/>
+              </Flex> 
+            </CardBody>
+          </Card>
 
         </HStack>
         </VStack>
