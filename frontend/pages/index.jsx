3f381a1b3b89333243ce6e2c4f7fc704b133d703
Nicolas Silva Pereira <njsilvapereira@gmail.com>
2023-05-18 22:58:26 -0400

    pagina reservar

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,19 +1,20 @@
 import Head from 'next/head'
 import { Box } from '@chakra-ui/react'
-import { Stack } from '@chakra-ui/react'
+import { Stack , Image} from '@chakra-ui/react'
 
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Sportify</title>
+        <title>Sportifyy</title>
         <meta name="description" content="Reserva de Canchas" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Box bg='black' w='100vw' h='100vh' p={4} color='white'>
         Sportify
+        <Image src='reserva/mbape.jpg' fallbackSrc='https://via.placeholder.com/150' />
       </Box>
     </>
   )
--- frontend/pages/reserva/reservas.jsx
+++ frontend/pages/reserva/reservas.jsx
@@ -1,31 +1,63 @@
 import React from 'react'
+import {SlOptionsVertical} from 'react-icons/sl'
+import {BsHouseDoor} from 'react-icons/bs'
+import Logo from './mbape.jpg'
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    Input,
-    Stack,
+    Box,
+    Button,
+    Flex,
+    Heading,
+    Icon,
+    Image,
+    Spacer,
+    BackgroundImage,
+    VStack,
+    Container,
+    HStack,
+    Text
   } from '@chakra-ui/react'
 
+ 
 const reservas= ()=> {
   return (
-    <Stack>
-        <Breadcrumb separator='-'>
-  <BreadcrumbItem>
-    <BreadcrumbLink href='#'>Home</BreadcrumbLink>
-  </BreadcrumbItem>
+    <Container  margin={'0'} p={'0'} maxW={'full'} maxH={'full'}>
+      <Image src='Sportify.jpg'/>
+      <HStack as="nav" p="10px" bg="#5D3C81">
+        <Heading color={'white'} >Sportify</Heading>
+        <Spacer/>
+        <Button colorScheme='teal' variant='unstyled' color={'white'} mx="20px">
+        <Icon as={BsHouseDoor} color={'white'} mx="2px"/>
+          Inicio
+        </Button>
+        <Button colorScheme='teal' variant='unstyled' color={'white'} mx="10px">
+        <Box><Icon as={SlOptionsVertical} color={'white'}/></Box>
+        </Button>
+      </HStack>
+      <VStack bg={"gray.300"} width={'full'} height={' container.lg'}>
+        <Heading my={'30px'} p={'15px'}>Selecciona el deporte </Heading>
+        <VStack spacing={'30px'} my={'25px'}>
+        <HStack spacing={'30px'}> 
+          <Box bg={'red.300'} w={'250px'} h={'256px'} onClick={()=>{alert("futbol")}}>futbolito</Box>
+          <Card bg={'red.300'} w={'250px'} h={'256px'}>
+            <CardHeader>
+              <Heading textAlign="center">Tenis</Heading>
+              <Box>
+              <Image src='Sportify.jpg'/>
+              </Box>
+            </CardHeader>
+          </Card>
+          <Box bg={'red.300'} w={'250px'} h={'256px'}>BasketBall</Box>
+        </HStack>
+        <HStack spacing={'30px'}>
+          <Box bg={'red.300'} w={'250px'} h={'256px'}>Padel</Box>
+          <Box bg={'red.300'} w={'250px'} h={'256px'}>Voleibol</Box>
 
-  <BreadcrumbItem>
-    <BreadcrumbLink href='#'>About</BreadcrumbLink>
-  </BreadcrumbItem>
-
-  <BreadcrumbItem isCurrentPage>
-    <BreadcrumbLink href='#'>Contact</BreadcrumbLink>
-  </BreadcrumbItem>
-  
-</Breadcrumb>
-<Input></Input>
-    </Stack>
+        </HStack>
+        </VStack>
+      </VStack>
+    </Container>
+    
   )
 }
 
