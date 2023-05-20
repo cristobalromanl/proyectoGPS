54a7c039b09edc5cb7f8ae5ce18c14204f95d987
MartinR1F <martin.riffo1801@alumnos.ubiobio.cl>
2023-05-20 03:05:23 -0400

    Probando tabla con canchas, solo visual, falta probar con datos de la bdd

--- frontend/pages/reserva/reservas.jsx
+++ frontend/pages/reserva/reservas.jsx
@@ -1,31 +1,88 @@
 import React from 'react'
+import {SlOptionsVertical} from 'react-icons/sl'
+import {BsHouseDoor} from 'react-icons/bs'
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    Input,
-    Stack,
+    Box,
+    Button,
+    Heading,
+    Icon,
+    Image,
+    Spacer,
+    Flex,
+    VStack,
+    Container,
+    HStack,
+    Text
   } from '@chakra-ui/react'
 
+  
 const reservas= ()=> {
   return (
-    <Stack>
-        <Breadcrumb separator='-'>
-  <BreadcrumbItem>
-    <BreadcrumbLink href='#'>Home</BreadcrumbLink>
-  </BreadcrumbItem>
+    <Container  margin={'0'} p={'0'} maxW={'full'} maxH={'full'}>
+      <HStack as="nav" p="10px" bg="#5D3C81">
+        <Heading color={'white'}><Image src='/img/Sportify.png' w={'110px'} h={'100'}/></Heading>
+        <Spacer/>
+        <Button colorScheme='teal' variant='unstyled' color={'white'} mx="100px" fontSize={'28px'} display={'flex'} >
+        <Icon as={BsHouseDoor} color={'white'} w={'48px'} h={'45px'}/>
+         <Text w={'79px'} h={'42'}>Inicio</Text>
+        </Button>
+        <Button colorScheme='teal' variant='unstyled' color={'white'} mx="20px">
+        <Box><Icon w={'45px'} h={'45px'} as={SlOptionsVertical} color={'white'}/></Box>
+        </Button>
+      </HStack>
+      <VStack bg={"#c08ee1"} width={'full'} height={' container.lg'}>
+        <Heading my={'30px'} p={'15px'}>Selecciona el deporte </Heading>
+        <VStack spacing={'30px'} my={'25px'}>
+        <HStack spacing={'30px'}> 
+        <Card bg={'#c08ee1'} w={'250px'} h={'252px'}transition={['0.3s', 'ease']}  boxShadow={'dark-lg'} cursor={'pointer'} onClick={()=>{location.href='/reserva/canchas'}}>
+            <CardBody>
+              <Flex direction={'column'} alignContent={'center'} justify={'center'}>
+                <Heading textAlign="center">Futbolito</Heading>
+                <Image mx={'29px'} h={'130px'} w={'140px'} src='https://cdn.discordapp.com/attachments/723251921393025028/1108953429147131955/asfasdf.png'/>
+              </Flex> 
+            </CardBody>
+          </Card>
+          <Card bg={'#c08ee1'} w={'250px'} h={'252px'}transition={['0.3s', 'ease']}  boxShadow={'dark-lg'} cursor={'pointer'} onClick={()=>{location.href='/reserva/canchas'}}>
+            <CardBody>
+              <Flex direction={'column'} alignContent={'center'} justify={'center'} >
+                <Heading textAlign="center">Tenis</Heading>
+                <Image mx={'29px'} h={'130px'} w={'140px'} src='https://cdn.discordapp.com/attachments/723251921393025028/1108953502291599411/tenis.png'/>
+              </Flex> 
+            </CardBody>
+          </Card>
+          <Card bg={'#c08ee1'} w={'250px'} h={'252px'} transition={['0.3s', 'ease']}  boxShadow={'dark-lg'} cursor={'pointer'}>
+            <CardBody>
+              <Flex direction={'column'} alignContent={'center'} justify={'center'} onClick={()=>{location.href='/reserva/canchas'}}>
+                <Heading textAlign="center">Basketball</Heading>
+                <Image mx={'29px'} h={'130px'} w={'140px'} src='https://cdn.discordapp.com/attachments/723251921393025028/1108953551474004058/i.png'/>
+              </Flex> 
+            </CardBody>
+          </Card>
+        </HStack>
+        <HStack spacing={'30px'}>
+        <Card bg={'#c08ee1'} w={'250px'} h={'252px'} transition={['0.3s', 'ease']}  boxShadow={'dark-lg'} cursor={'pointer'} onClick={()=>{location.href='/reserva/canchas'}}>
+            <CardBody>
+              <Flex direction={'column'} alignContent={'center'} justify={'center'}>
+                <Heading textAlign="center">Padel</Heading>
+                <Image mx={'56px'} h={'130px'} w={'140px'} src='https://cdn.discordapp.com/attachments/723251921393025028/1108953751391309874/raqueta-de-padel.png'/>
+              </Flex> 
+            </CardBody>
+          </Card>
+          <Card bg={'#c08ee1'} w={'250px'} h={'252px'} transition={['0.3s', 'ease']}  boxShadow={'dark-lg'} cursor={'pointer'} onClick={()=>{location.href='/reserva/canchas'}}>
+            <CardBody>
+              <Flex direction={'column'} alignContent={'center'} justify={'center'}>
+                <Heading textAlign="center">Voley</Heading>
+                <Image mx={'29px'} h={'130px'} w={'140px'} src='https://cdn.discordapp.com/attachments/723251921393025028/1108953959995035688/cuadra.png'/>
+              </Flex> 
+            </CardBody>
+          </Card>
 
-  <BreadcrumbItem>
-    <BreadcrumbLink href='#'>About</BreadcrumbLink>
-  </BreadcrumbItem>
-
-  <BreadcrumbItem isCurrentPage>
-    <BreadcrumbLink href='#'>Contact</BreadcrumbLink>
-  </BreadcrumbItem>
-  
-</Breadcrumb>
-<Input></Input>
-    </Stack>
+        </HStack>
+        </VStack>
+      </VStack>
+    </Container>
+    
   )
 }
 
