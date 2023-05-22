cc3fdc6583bb5fe5ec1c6f29173c4225a69021c0
Diego V <diego.valderrama1501@alumnos.ubiobio.cl>
2023-05-21 23:01:39 -0400

    implementacion menu tipo fifa v1 mas login

--- frontend/pages/_document.jsx
+++ frontend/pages/_document.jsx
@@ -1,16 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
+import { ColorModeScript, Image, Flex} from '@chakra-ui/react'
 import theme from '@/styles/theme'
 
+
 export default function Document() {
+
   return (
     <Html lang="es">
       <Head />
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
-        <h1>Sportify</h1>
-        <h2><b>Ciudad deportiva</b></h2>
+        
+        
+        {/* <Image width="100%" src='/fondoCancha.jpeg' objectFit="cover"></Image> */}
+        
+      
         <NextScript />
       </body>
     </Html>
