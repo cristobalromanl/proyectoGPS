01d46c4cea15d20a8b705b84d3219b0c620c172a
Cristobal Roman <romanleiva1998@gmail.com>
2023-08-09 05:00:10 -0400

    filtrado clubs desde bdd

--- frontend/pages/listaclubs.jsx
+++ frontend/pages/listaclubs.jsx
@@ -10,80 +10,32 @@ import {
   Th,
   Td,
   Input,
+  useToast,
 } from "@chakra-ui/react";
 import HomeLayout from "@/components/HomeLayout";
-import { getAll } from "@/services/categories";
 
-const overviewList = [
-  {
-    id: 1,
-    label: "Equipo Lince",
-    logo: "/logosequipos/team1.png",
-    registeredAt:"10-08-2023",
-    sportType: "Fútbol"
-  },
-  {
-    id: 2,
-    label: "Equipo Dragon",
-    logo: "/logosequipos/team2.png",
-    registeredAt:"12-07-2023",
-    sportType: "Fútbol"
-  },
-  {
-    id: 3,
-    label: "Equipo Bicho",
-    logo: "/logosequipos/team3.png",
-    registeredAt:"10-09-2023",
-    sportType: "Padel"
-  },
-  {
-    id: 4,
-    label: "Equipo L",
-    logo: "/logosequipos/team4.png",
-    registeredAt:"22-09-2023",
-    sportType: "Tenis"
-  },
-  {
-    id: 5,
-    label: "Equipo Partisano",
-    logo: "/logosequipos/team5.png",
-    registeredAt:"15-08-2023",
-    sportType: "Padel"
-  },
-  {
-    id: 6,
-    label: "Equipo Yuste",
-    logo: "/logosequipos/team6.png",
-    registeredAt:"01-09-2023",
-    sportType: "Tenis"
-  },
-  {
-    id: 7,
-    label: "Equipo MMJ",
-    logo: "/logosequipos/team7.png",
-    registeredAt:"23-07-2023",
-    sportType: "Fútbol"
-  },
-  {
-    id: 8,
-    label: "Equipo Kiku",
-    logo: "/logosequipos/team8.png",
-    registeredAt:"31-07-2023",
-    sportType: "Padel"
-  },
-];
+import { useRouter } from "next/router";
+import { getAll } from "@/services/clubs";
 
 export default function ReservasPage() {
-  const [categories, setCategories] = useState([]);
-  const [filter, setFilter] = useState("");;
+  const toast = useToast();
+  const router = useRouter();
+
+  const [clubs, setClubs] = useState([]);
+  const [filter, setFilter] = useState("");
 
   const handleChange = (e) => {
-    setFilter(e.target.value); // Actualizar el estado del filtro
+    setFilter(e.target.value);
+  };
+
+  const formatDate = (date) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(date).toLocaleDateString(undefined, options);
   };
 
   useEffect(() => {
     getAll()
-      .then((categorias) => setCategories(categorias))
+      .then((fetchedClubs) => setClubs(fetchedClubs))
       .catch((_error) =>
         toast({
           title: "Error al obtener los datos. Intentelo más tarde.",
@@ -93,12 +45,11 @@ export default function ReservasPage() {
       );
   }, []);
 
-  // Filtrar equipos basados en los valores seleccionados
-  const filteredTeams = overviewList.filter((team) => {
-    const filterText = filter.toLowerCase(); // Convertir el filtro a minúsculas
+  const filteredClubs = clubs.filter((club) => {
+    const filterText = filter.toLowerCase();
     return (
-      team.label.toLowerCase().includes(filterText) ||
-      team.registeredAt.includes(filterText) || team.sportType.toLowerCase().includes(filterText)
+      club.name.toLowerCase().includes(filterText) ||
+      formatDate(club.createdAt).includes(filterText)
     );
   });
 
@@ -111,33 +62,38 @@ export default function ReservasPage() {
         <Box mb={4}>
           <Input
             type="text"
-            placeholder="Filtrar tabla por nombre del club o fecha"
+            placeholder="Filtrar tabla"
             value={filter}
             onChange={handleChange}
           />
         </Box>
-        <Table variant="striped">
-          <Thead>
-            <Tr>
-              <Th>Equipo</Th>
-              <Th>Logo</Th>
-              <Th>Fecha de Creación</Th>
-              <Th>Tipo de Deporte</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {filteredTeams.map((team) => (      //Mostrar los datos en la tabla
-              <Tr key={team.id}>
-                <Td>{team.label}</Td>
-                <Td>
-                  <Image src={team.logo} alt={`${team.label} Logo`} height="50px" width="50px" />
-                </Td>
-                <Td>{team.registeredAt}</Td>
-                <Td>{team.sportType}</Td>
+        <Box mb={4}>
+          <Table variant="striped">
+            <Thead>
+              <Tr>
+                <Th>Equipo</Th>
+                <Th>Logo</Th>
+                <Th>Fecha de Creación</Th>
               </Tr>
-            ))}
-          </Tbody>
-        </Table>
+            </Thead>
+            <Tbody>
+              {filteredClubs.map((club) => (
+                <Tr key={club.id}>
+                  <Td>{club.name}</Td>
+                  <Td>
+                    <Image
+                      src={club.logoPath}
+                      alt={`${club.name} Logo`}
+                      height="100px"
+                      width="100px"
+                    />
+                  </Td>
+                  <Td>{formatDate(club.createdAt)}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
       </Box>
     </HomeLayout>
   );
