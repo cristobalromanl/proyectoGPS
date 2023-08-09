ace71a6cf32bc928a9bf7177f16c374735303d5a
Cristobal Roman <romanleiva1998@gmail.com>
2023-08-09 05:00:28 -0400

    creacion de clubs service

---
+++ frontend/services/clubs.js
@@ -0,0 +1,70 @@
+import axios from './axios'
+
+export const getAll = async () => {
+  try {
+    const res = await axios.get('/api/clubs')
+
+    res.data.sort((a, b) => (
+      new Date(b.createdAt) - new Date(a.createdAt)
+    ))
+
+    return res.data
+  } catch (error) {
+    const newError = new Error(error.response.statusText)
+    newError.status = error.response.status
+
+    throw newError
+  }
+}
+
+export const getOne = async (id) => {
+  try {
+    const res = await axios.get(`/api/clubs/${id}`)
+
+    return res.data
+  } catch (error) {
+    const newError = new Error(error.response.statusText)
+    newError.status = error.response.status
+
+    throw newError
+  }
+}
+
+export const create = async (clubs) => {
+  try {
+    const res = await axios.post('/api/clubs', clubs)
+
+    return res.data
+  } catch (error) {
+    const newError = new Error(error.response.statusText)
+    newError.status = error.response.status
+
+    throw newError
+  }
+}
+
+export const update = async (clubs) => {
+  try {
+    const res = await axios.put(`/api/fields/${clubs.id}`, clubs)
+
+    return res.data
+  } catch (error) {
+    const newError = new Error(error.response.statusText)
+    newError.status = error.response.status
+
+    throw newError
+  }
+}
+
+export const remove = async (id) => {
+  try {
+    const res = await axios.delete(`/api/clubs/${id}`)
+
+    return res.data
+  } catch (error) {
+    const newError = new Error(error.response.statusText)
+    newError.status = error.response.status
+
+    throw newError
+  }
+}
